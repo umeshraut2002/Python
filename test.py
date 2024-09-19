1a2adea8561c8d38c1961ba5d250f19a9e142be8
umesh raut <uraut6044@gmail.com>
2024-09-19 12:56:03 +0530

    varibles methods control flow

--- test.py
+++ test.py
@@ -4,3 +4,17 @@ num1 = 22
 num2 = 34
 
 print(num1 + num2)
+
+def greet(name):
+    print("hello " + name + " how are you ?")
+
+greet("Bob")
+
+age = 17
+
+if age > 18:
+    print("you can drive")
+elif age < 18:
+    print("you can not drive")
+else :
+  print("asked to your parrents")
