a012d576bdaa979f9271419649de9a04f6a10de3
umesh raut <uraut6044@gmail.com>
2024-09-20 12:03:43 +0530

    strings methods

---
+++ strings.py
@@ -0,0 +1,22 @@
+fullNmae = "Umesh Babanraoji Raut"
+
+# for i in fullNmae:
+#     print(i)
+
+# comprehesion of strings 
+
+# list = [char for char in fullNmae]
+
+
+# lets find in string of j
+
+for i in fullNmae:
+    print(i)
+
+
+# print(fullNmae.find("j"))
+
+# slicing 
+
+print(fullNmae[0:16])
+
