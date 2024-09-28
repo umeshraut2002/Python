bb403c0ce5356b7c42238fee45c5a583e0d9d93f
umesh raut <uraut6044@gmail.com>
2024-09-28 09:47:07 +0530

    classes and objects are updated

--- classesAndObject.py
+++ classesAndObject.py
@@ -1,3 +1,31 @@
 class employee:
     def __init__(self, id = None, name = None, salary = None):
-        
+        self.id = id
+        self.name = name
+        self.salary = salary
+
+    # this is are the setters 
+
+    def setId(self, id):
+        self.id = id
+
+    def setName(self, name):
+        self.name = name
+
+    def setSalary(self, salary):
+        self.salary = salary
+    
+    # this is are the getters 
+
+    def getId(self):
+        return self.id
+    
+    def getName(self):
+        return self.name
+    
+    def getSalary(self):
+        return self.salary
+    
+
+emp1 = employee(1, "umesh", 150000000);
+print(emp1.getId(), emp1.getName(), emp1.getSalary())
