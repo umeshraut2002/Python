a0508dad8ffb0aa912d4d1b128ebddd5dc9989d2
umesh raut <uraut6044@gmail.com>
2024-09-28 09:30:09 +0530

    class and object test numpy valid anagram

---
+++ testnumpy.py
@@ -0,0 +1,11 @@
+# import numpy as np
+
+# a = np.array([1,2,3,4,5])
+
+# print(a)
+
+from numpy import *
+
+arr = ["a", "b", "c", "d"]
+
+print(arr)
---
+++ valid_anagram.py
@@ -0,0 +1,8 @@
+
+s = "anagram" 
+t = "nagrama"
+
+if sorted(s) == sorted(t):
+    print(True)
+else:
+    print(False)
